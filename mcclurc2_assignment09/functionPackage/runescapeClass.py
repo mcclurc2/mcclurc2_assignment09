303d3b5af663e7e6a662121ea60f8e78addd204f
Chase McClure <Chase McClure@DESKTOP-MIJ379N.wifi.uc.edu>
2024-04-02 13:30:05 -0400

    added documentation

--- mcclurc2_assignment09/functionPackage/runescapeClass.py
+++ mcclurc2_assignment09/functionPackage/runescapeClass.py
@@ -1,5 +1,16 @@
-import requests
+# Name:Chase McClure, Cameron Hogan, Riley
+# email:mcclurc2@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: april 4 
+# Course/Section: ADV APP DEV (001)
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: this assignment is to see if we can get an api url and to read informatoin. to take the url run code to read and get important informaiation from it as well to be able to follow directions and do what the assignment says
+# Brief Description of what this module does. Do not copy/paste from a previous assignment. Put some thought into this. this module shows us how to connect to an api from eclipse and to read it rfom here
+# Citations:canvas slides
 
+
+
+import requests
 import json
 
  
@@ -27,3 +38,5 @@ def extractInfo():
    
 
     parsed_json = json.loads(json_string) # Now we have a python dictionary
+    
+    
